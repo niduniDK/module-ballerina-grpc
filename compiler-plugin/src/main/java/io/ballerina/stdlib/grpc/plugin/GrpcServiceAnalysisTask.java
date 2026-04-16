c5289aba184c6ad5b447faa72b03a6ff7fa33402
niduniDK <nidunikasige@gmail.com>
2026-04-16 09:06:24 +0530

    Add endpoint details extraction and proto file export logic

---
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/grpc/plugin/GrpcServiceAnalysisTask.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.stdlib.grpc.plugin;
+
+import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
+import io.ballerina.projects.plugins.AnalysisTask;
+import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
+import io.ballerina.stdlib.grpc.plugin.endpointyaml.generator.EndpointYamlGenerator;
+import io.ballerina.stdlib.grpc.plugin.endpointyaml.generator.ProtoFileExporter;
+import io.ballerina.tools.diagnostics.DiagnosticFactory;
+import io.ballerina.tools.diagnostics.DiagnosticInfo;
+import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+
+import java.io.IOException;
+
+public class GrpcServiceAnalysisTask implements AnalysisTask<SyntaxNodeAnalysisContext> {
+    @Override
+    public void perform(SyntaxNodeAnalysisContext context) {
+        ServiceDeclarationNode serviceNode = (ServiceDeclarationNode) context.node();
+        EndpointYamlGenerator endpointYamlGeneratorGrpc = new EndpointYamlGenerator(context);
+        ProtoFileExporter protoFileExporter = new ProtoFileExporter(context);
+        try {
+            endpointYamlGeneratorGrpc.writeEndpointYaml();
+            protoFileExporter.exportProtoFile();
+        } catch (IOException e) {
+            context.reportDiagnostic(
+                DiagnosticFactory.createDiagnostic(
+                    new DiagnosticInfo(
+                        "GRPC_PLUGIN_DEBUG",
+                        "[grpc-plugin] " +
+                        e.getMessage(),
+                        DiagnosticSeverity.ERROR
+                    ),
+                    serviceNode.location()
+                )
+            );
+        }
+
+    }
+}
---
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/grpc/plugin/endpointyaml/generator/ProtoFileExporter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2026, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.stdlib.grpc.plugin.endpointyaml.generator;
+
+import com.google.protobuf.DescriptorProtos;
+import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
+import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ProtoFileExporter {
+    private static final String ARTIFACT = "artifact";
+    private static final String TARGET = "target";
+    private final SyntaxNodeAnalysisContext context;
+    private final ServiceDeclarationNode serviceNode;
+    private final SemanticModel semanticModel;
+
+    public ProtoFileExporter(SyntaxNodeAnalysisContext context) {
+        this.context = context;
+        this.serviceNode = (ServiceDeclarationNode) this.context.node();
+        this.semanticModel = this.context.semanticModel();
+    }
+    public void exportProtoFile() throws IOException {
+        ServiceDescExtractor descExtractor = new ServiceDescExtractor(this.serviceNode, this.semanticModel, context);
+        AtomicReference<DescriptorProtos.FileDescriptorProto> fds = descExtractor.extract();
+        FileNameGeneratorUtil fileNameGeneratorUtil = new FileNameGeneratorUtil(this.context);
+        String filePath = fileNameGeneratorUtil.getFileName();
+        Path path = Paths.get(TARGET, ARTIFACT, filePath).toAbsolutePath();
+        ProtoFileWriter.writeToProtoFile(fds, path.toString());
+    }
+}
